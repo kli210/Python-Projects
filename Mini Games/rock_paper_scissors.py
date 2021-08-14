796280596d4865e4dc4aabe4e6928d5fd051c661
kli210 <87552132+kli210@users.noreply.github.com>
2021-08-14 11:06:42 -0400

    taking the edge off some edge cases..

--- Mini Games/rock_paper_scissors.py
+++ Mini Games/rock_paper_scissors.py
@@ -1,10 +1,29 @@
+# INSTRUCTIONS:
+# Easy-to-play game against a computer. 
+# You and the computer choose between Rock, Paper, and Scissors,
+# and on the count of three, 
+# both players' choice are simultaneously revealed.
+# Rock beats Scissors, Paper beats Rock, and Scissors beat Paper.
+# Let the games begin!
+
 import random
 
 def play():
+    repeat = False
     while True:
-        user_action= input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors.\n")
-        computer_action = random.choice(['r', 'p', 's'])
-        print(f"\nYou chose {user_action}, the computer chose {computer_action}")
+        try:
+            if repeat:
+                break
+            user_action= input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors.\n")
+            computer_action = random.choice(['r', 'p', 's'])
+            user_action.isalpha()
+            if user_action.lower() not in ['r', 'p', 's']:
+                print("Please type 'r' for rock, 'p' for paper, 's' for scissors.")
+                continue
+        except:
+            print("Please enter a letter. Type 'r' for rock, 'p' for paper, 's' for scissors.")
+            continue
+        print(f"\n1..2..3!\nYou chose {user_action}, the computer chose {computer_action}.")
         # r > s, s > p, p > r.
         if user_action == computer_action:
             print(f"Both players selected {user_action}. It's a tie!")
@@ -23,12 +42,31 @@ def play():
                 print("Scissors cut paper! You win!")
             else:
                 print("Rock smashes scissors! You lose.")
+    
+        while True:
+            play_again = input("\nPlay again? (y/n): ")
+            if play_again.lower() == "y":
+                print("Nice!")
+                break
+            elif play_again.lower() == "n":
+                print("\nGood game! See you again.\n")
+                repeat = True
+                break
+            else:
+                print("Please type 'y' for yes, or 'n' for no.")
+                continue
         
-        play_again = input("\nPlay again? (y/n): ")
-        if play_again.lower() != "y":
-            print("\nGood game! See you again.\n")
-            break
-        else:
-            print("Nice!")
+
+
+
+print("Welcome to this easy-to-play game against a computer.\
+    \nYou and the computer choose between Rock, Paper, and Scissors,\
+    \nand on the count of three,\
+    \nboth players' choice are simultaneously revealed.\
+    \nRock beats Scissors, Paper beats Rock, and Scissors beat Paper.\
+    \nLet the games begin!'\n")
+print("Hi there, what's your name?")
+name = input()
+print(f"Okay, {name}!")
 
 play()
